895791090d7cda44262bd3993ce2226dd3587e19
Ian Halpern <ian@ian-desktop>
2009-04-11 13:09:32 -0400

    Updated files in preperation for release.

--- Impulse.py
+++ Impulse.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 #
 #+   Copyright (c) 2009 Ian Halpern
-#@   http://Spectrolet.com
+#@   http://impulse.ian-halpern.com
 #
-#    This program is free software: you can redistribute it and/or modify
+#    This file is part of Impulse.
+#
+#    Impulse is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
+#    Impulse is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with Impulse.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import os, sys, gtk, cairo, time, random, struct, signal, gobject
 from gtk import gdk
